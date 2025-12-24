595840e8f2495fa3e860a8c4593a951421c8d8cf
sujithprem <sujithprem872gmail.com>
2025-12-24 10:17:34 +0530

    models user fix

--- server/models/user.js
+++ server/models/user.js
@@ -1,12 +1,13 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-    _id: {type: String, required: true},
-    name: {type: String, required: true},
-    email: {type: String, required: true},
-    image: {type: String, required: true}
-})
+  _id: { type: String, required: true },
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  image: { type: String, required: true },
+});
 
-const User = mongoose.model('User', userSchema)
+// ✅ Prevent model overwrite in serverless
+const User = mongoose.models.User || mongoose.model("User", userSchema);
 
 export default User;
