e7c7292629e6ab8d4b6222b4ea4eb8ea3b582998
sujithprem <sujithprem872gmail.com>
2026-02-11 20:52:18 +0530

    small

--- server/server.js
+++ server/server.js
@@ -1,23 +1,16 @@
 import express from "express";
-import cors from "cors";
-import 'dotenv/config.js';
-import connectDB from "./configs/db.js";
-import { clerkMiddleware } from '@clerk/express';
-import { serve } from 'inngest/express';
-import { inngest, functions } from "./inngest/index.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
-const port = 3000;
-
-await connectDB();
-
-// Middleware
 app.use(express.json());
-app.use(cors());
-app.use(clerkMiddleware());
 
-// API Routes
-app.get('/', (req, res) => res.send('Server is live!'))
-app.use('/api/inngest', serve({ client: inngest, functions }))
+app.get("/", (req, res) => {
+  res.send("Backend running 🚀");
+});
 
-app.listen(port, () => console.log(`Server listening at http://localhost:${port}`));
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
