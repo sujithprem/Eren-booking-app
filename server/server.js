79ccda740f42b8065b5c406b14818f62ed30b338
sujithprem <sujithprem872gmail.com>
2026-02-13 09:55:35 +0530

    pro

--- server/server.js
+++ server/server.js
@@ -1,14 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
+import {inngest, functions} from "./inngest/index.js";
+import showRouter from "./routes/showRoutes.js";
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Backend running 🚀");
-});
+app.get("/", (req, res) => { res.send("Backend running 🚀")});
+app.use("/api/inngest", inngest.serve(functions));
+app.use('/api/show', showRouter)
 
 const PORT = 5000;
 app.listen(PORT, () => {
